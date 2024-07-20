c4d7252757ef4dd81432369eee037e792e1fad66
Grotball <140941449+Grotball@users.noreply.github.com>
2024-07-20 11:17:04 +0800

    Update NBodySystem to use load balancer

--- src/nbody_system.cpp
+++ src/nbody_system.cpp
@@ -1,6 +1,11 @@
 #include "nbody_system.h"
 
-NBodySystem::NBodySystem(const int numBodies, const bool isMaster, const bool isWorker, const MPI_Comm workerComm) : numBodies(numBodies), isMaster(isMaster), isWorker(isWorker), workerComm(workerComm)
+NBodySystem::NBodySystem(const int numBodies, const bool isMaster, const bool isWorker, const MPI_Comm workerComm, const MPI_Datatype stridedComponentVecType) : 
+        numBodies(numBodies), 
+        isMaster(isMaster), 
+        isWorker(isWorker), 
+        workerComm(workerComm),
+        stridedComponentVecType(stridedComponentVecType)
 {
     pos = new float[numBodies * 3];
     vel = new float[numBodies * 3];
@@ -25,12 +30,91 @@ NBodySystem::NBodySystem(const int numBodies, const bool isMaster, const bool is
             mass[i] =  100.0f / (numBodies * G);
         }
     }
+    MPI_Bcast(pos, 3 * numBodies, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(vel, 3 * numBodies, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(mass, numBodies, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    loadBalancer = std::make_unique<StaticBalancer>(isWorker, workerComm);
 }
 
 void NBodySystem::update(const float dt)
 {
-    //TODO: implement.
-    // would like a dedicated load balancing class.
+    loadBalancer->newWorkInit(numBodies);
+
+    std::vector<MPI_Request> requests;
+
+    int workerRank;
+    MPI_Comm_rank(workerComm, &workerRank);
+
+    while (isWorker && !loadBalancer->isFinished())
+    {
+        WorkTask workTask = loadBalancer->getWork();
+
+        bool isReceiveTask = workTask.workSource != workerRank && workTask.workSource >= 0;
+        if (isReceiveTask)
+        {
+            MPI_Request request;
+            MPI_Ibcast(acc+workTask.workStart, workTask.workLength, stridedComponentVecType, workTask.workSource, workerComm, &request);
+            requests.emplace_back(request);
+        }
+        else
+        {
+            int32_t lo = workTask.workStart;
+            int32_t hi = lo + workTask.workLength;
+
+            computeAccelleration(acc, pos, mass, lo, hi, numBodies);
+
+            MPI_Request request;
+            MPI_Ibcast(acc+workTask.workStart, workTask.workLength, stridedComponentVecType, workerRank, workerComm, &request);
+            requests.emplace_back(request);
+        }
+    }
+    
+    if (isWorker)
+    {
+        MPI_Waitall(requests.size(), requests.data(), MPI_STATUSES_IGNORE);
+    }
+
+    requests.clear();
+
+    loadBalancer->newWorkInit(numBodies);
+
+    while (isWorker && !loadBalancer->isFinished())
+    {
+        WorkTask workTask = loadBalancer->getWork();
+
+        bool isReceiveTask = workTask.workSource != workerRank && workTask.workSource >= 0;
+        if (isReceiveTask)
+        {
+            requests.push_back(0);
+            MPI_Ibcast(pos+workTask.workStart, workTask.workLength, stridedComponentVecType, workTask.workSource, workerComm, &requests.back());
+            requests.push_back(0);
+            MPI_Ibcast(vel+workTask.workStart, workTask.workLength, stridedComponentVecType, workTask.workSource, workerComm, &requests.back());
+        }
+        else
+        {
+            int32_t lo = workTask.workStart;
+            int32_t hi = lo + workTask.workLength;
+
+
+            for (int k = 0; k < 3; k++)
+            {
+                auto i0 = k * numBodies;
+                for (auto i = lo; i < hi; i++)
+                {
+                    pos[i0 + i] += vel[i0 + i] * dt;
+                    vel[i0 + i] += acc[i0 + i] * dt;
+                }
+            }
+
+            requests.push_back(0);
+            MPI_Ibcast(pos+workTask.workStart, workTask.workLength, stridedComponentVecType, workerRank, workerComm, &requests.back());
+            requests.push_back(0);
+            MPI_Ibcast(vel+workTask.workStart, workTask.workLength, stridedComponentVecType, workerRank, workerComm, &requests.back());
+        }
+    }
+
+    MPI_Waitall(requests.size(), requests.data(), MPI_STATUSES_IGNORE);
 }
 
 NBodySystem::~NBodySystem()
--- src/nbody_system.h
+++ src/nbody_system.h
@@ -1,20 +1,26 @@
 #pragma once
 #include <mpi.h>
 #include <random>
+#include <memory>
 #include "nbody.h"
+#include "load_balancer.h"
 
+
+// NOTE: This class will likely experience major changes.
 class NBodySystem
 {
     const int numBodies;
     const bool isMaster;
     const bool isWorker;
+    MPI_Comm workerComm;
+    MPI_Datatype stridedComponentVecType;
+    std::unique_ptr<LoadBalancer> loadBalancer;
+    public:
     float* pos;
     float* vel;
     float* acc;
     float* mass;
-    MPI_Comm workerComm;
-    public:
-    NBodySystem(const int numBodies, const bool isMaster, const bool isWorker, const MPI_Comm workerComm);
+    NBodySystem(const int numBodies, const bool isMaster, const bool isWorker, const MPI_Comm workerComm, const MPI_Datatype stridedComponentVecType);
     void update(const float dt);
     ~NBodySystem();
 };
