f5f1f6b55950f477e271dffb337f58d03167e5f8
Grotball <140941449+Grotball@users.noreply.github.com>
2024-07-22 20:19:42 +0800

    Add quaternion to rotation matrix

--- src/quaternion.cpp
+++ src/quaternion.cpp
@@ -76,3 +76,35 @@ Quaternion conjugate(const Quaternion& quaternion)
     Quaternion q {quaternion.w, -quaternion.x, -quaternion.y, -quaternion.z};
     return q;
 }
+
+void computeQuaternionRotationMatrix(float R[16], const Quaternion& rot)
+{
+    const float w2 = rot.w * rot.w;
+    const float x2 = rot.x * rot.x;
+    const float y2 = rot.y * rot.y;
+    const float z2 = rot.z * rot.z;
+    
+    const float k = 1.0f / (w2 + x2 + y2 + z2);
+    
+    R[0] = 1 - 2 * k * (y2 + z2);
+    R[1] = 2 * k * (rot.x * rot.y + rot.w * rot.z);
+    R[2] = 2 * k * (rot.x * rot.z - rot.w * rot.y);
+    R[3] = 0;
+    
+    R[4] = 2 * k * (rot.x * rot.y - rot.w * rot.z);
+    R[5] = 1 - 2 * k * (x2 + z2);
+    R[6] = 2 * k * (rot.y * rot.z + rot.w * rot.x);
+    R[7] = 0;
+    
+    R[8] = 2 * k * (rot.x * rot.z + rot.w * rot.y);
+    R[9] = 2 * k * (rot.y * rot.z - rot.w * rot.x);
+    R[10] = 1 - 2 * k * (x2 + y2);
+    R[11] = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        R[12 + i] = 0;
+    }
+
+    R[15] = 1;
+}
